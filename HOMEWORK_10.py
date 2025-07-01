5766f1a7e2fa36aaa808c7205cfadb11b505c8b1
Makkenzie <mak.svirilin@gmail.com>
2025-07-01 14:19:57 +0300

    Добавил задание

---
+++ HOMEWORK_10.py
@@ -0,0 +1,17 @@
+# ЗАДАЧА:
+# 🦄 “Превращение единорогов в радугу”
+#
+# Вам дан список единорогов, каждый представлен в виде словаря с полем color.
+# Напишите функцию unicorns_to_rainbows(unicorns: list[dict]) -> list[str],
+# которая превращает каждого единорога в строку:
+# "🌈 Rainbow unicorn of color COLOR", где COLOR — цвет единорога.
+#
+# Пример:
+unicorns = [{"color": "pink"}, {"color": "blue"}, {"color": "sparkly"}]
+
+# результат:
+# [
+#   "🌈 Rainbow unicorn of color pink",
+#   "🌈 Rainbow unicorn of color blue",
+#   "🌈 Rainbow unicorn of color sparkly"
+# ]
